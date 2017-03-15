a8966028d02a47665bee7d78339b0f560cbd4fcb
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2017-03-15 14:59:17 +0100

    Intial Commit

---
+++ LeCoinFlip/DataLayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace LeCoinFlip
+{
+	public class DataLayer
+	{
+		private int spinTimeLeft = 0;
+		private static string APIendPoint = "http://192.168.1.25:3000/";
+		private Socket DataLayerSocket = IO.Socket(APIendPoint);
+
+		public DataLayer()
+		{
+
+		}
+
+
+		//DataLayerSocket.On("countDown", data =>
+		//{
+
+		//});
+
+
+		public int getSpinTime()
+		{
+			return spinTimeLeft;
+		}
+	}
+}
---
+++ LeCoinFlip/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Quobject.SocketIoClientDotNet.Client;
+using System;
+using Newtonsoft.Json.Linq;
+using Android.Webkit;
+using Android.Content.PM;
+
+namespace LeCoinFlip
+{
+	[Activity(Label = "Le Coin Flip", MainLauncher = true, Icon = "@mipmap/icon", ScreenOrientation = ScreenOrientation.Portrait)]
+
+	public class MainActivity : Activity
+	{
+		public TextView countDown;
+
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
+			TextView connectedStatus = FindViewById<TextView>(Resource.Id.statusText);
+			countDown = FindViewById<TextView>(Resource.Id.countDown);
+			WebView webView = FindViewById<WebView>(Resource.Id.webView);
+
+			webView.Settings.JavaScriptEnabled = true;
+			webView.LoadUrl("http://192.168.1.25:3000/spinner");
+
+			var socket = IO.Socket("http://192.168.1.25:3000/");
+
+			socket.On("countDown", data =>
+			{
+				//timeLeft = data; //json.Value<int>("time");
+				editCountDown("Time to next spin: " + data);
+			});
+
+			socket.On("spin", data =>
+			{
+				editCountDown("Time to next spin: Spinning...");
+			});
+
+			socket.On(Socket.EVENT_CONNECT, () =>
+			{
+				RunOnUiThread(() =>
+				{
+					connectedStatus.Text = "Status: Connected";
+				});
+			});
+
+			socket.On(Socket.EVENT_DISCONNECT, () =>
+			{
+				RunOnUiThread(() =>
+				{
+					connectedStatus.Text = "Status: Not connected";
+				});
+			});
+		}
+
+		public void editCountDown(string text)
+		{
+			RunOnUiThread(() =>
+			{
+				countDown.Text = text;
+			});
+		}
+	}
+}
